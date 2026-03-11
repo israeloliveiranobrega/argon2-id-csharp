110d747c4a5e38485ffb93a376ca2d72f928056e
Israel Mendes <135517129+israeloliveiranobrega@users.noreply.github.com>
2026-03-11 09:06:12 -0300

    V 1.0.0

---
+++ src/Argon2IDHashSplit.cs
@@ -0,0 +1,11 @@
+﻿namespace Argon2ID;
+
+internal class Argon2IDHashSplit
+{
+    public int MemoryCost { get; init; }
+    public int TimeCost { get; init; }
+    public int Parallelism { get; init; }
+    public string Salt { get; init; } = string.Empty;
+    public string Hash { get; init; } = string.Empty;
+    public int HashSize { get; init; }
+}
